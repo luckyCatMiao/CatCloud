1a5c3e392d2733e7fc62332d724b9faacd00b125
MiaoMiao <360382992@qq.com>
2017-07-13 11:51:59 +0800

    Signed-off-by: MiaoMiao <360382992@qq.com>

---
+++ src/main/java/Main/ClientMain.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		
+		while(true)
+		{
+			System.out.println("666");
+		}
+	}
+}
---
+++ src/main/java/Main/ServerMain.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class ServerMain {
+
+	public static void main(String[] args) {
+		
+		while(true)
+		{
+			System.out.println(555);
+		}
+	}
+}
